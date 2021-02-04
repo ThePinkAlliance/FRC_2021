b5faa24d7ead5281ef3664fb7b57daacd8539a92
Zlannon <Zlannon@yahoo.com>
2021-02-04 18:03:31 -0500

    Climbers deleted

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -8,9 +8,6 @@ import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import frc.robot.commands.AutoDriveAndShoot;
 import frc.robot.commands.AutoShootOnly;
-import frc.robot.commands.ClimbersManual;
-import frc.robot.commands.ClimbersUnlock;
-import frc.robot.commands.ClimbersUp;
 import frc.robot.commands.CollectorManual;
 import frc.robot.commands.ConveyorAutomated;
 import frc.robot.commands.DriveManual;
@@ -26,7 +23,6 @@ import frc.robot.commands.ShooterMidAutomatic;
 import frc.robot.commands.SpinUpAndAim;
 import frc.robot.subsystems.Shooter;
 import frc.robot.subsystems.Base;
-import frc.robot.subsystems.Climber;
 import frc.robot.subsystems.Collector;
 import frc.robot.subsystems.Conveyor;
 import frc.robot.subsystems.Lights;
@@ -36,7 +32,6 @@ public class RobotContainer {
   private final Base      m_base        = new Base();
   private final Shooter   m_shooter     = new Shooter();
   private final Collector m_collector   = new Collector();
-  private final Climber   m_climber     = new Climber();
   private final Conveyor  m_conveyor    = new Conveyor();
   private final Lights    m_lights      = new Lights();
 
@@ -54,12 +49,9 @@ public class RobotContainer {
   public static int conveyorBeltCANID     = 62; // Brushless
   public static int shooterRotateCANID    = 60; // Brushed
   public static int shooterFlywheelCANID  = 31; // Brushless
-  public static int climberLeftCANID      = 21; // Brushless
-  public static int climberRightCANID     = 13; // Brushless
   public static int panelSpinnerCANID     = 30; // Brushed
   
   // Solenoid Ports
-  public static int climberLeftSolPort = 0;
   public static int collectorSolPort = 1;
  
   // PWM Ports
@@ -71,8 +63,6 @@ public class RobotContainer {
   public static int breakbeam1DIOPort = 9;
   public static int breakbeam2DIOPort = 8;
   public static int breakbeam3DIOPort = 5;
-  public static int climberLeftLowLimitSwitch = 6;
-  public static int climberRightLowLimitSwitch = 7;
 
   // Controller Ports
   Joystick mainJS = new Joystick(0);
@@ -86,10 +76,6 @@ public class RobotContainer {
   }
 
   private void configureOperatorController() {
-    m_climber.setDefaultCommand(new ClimbersManual(m_climber, () -> gunnerJS.getRawAxis(1), () -> gunnerJS.getRawAxis(3)));
-    new JoystickButton(gunnerJS, 1).whenPressed(new ClimbersUp(m_climber, Constants.climbToLeft));
-    new JoystickButton(gunnerJS, 2).whenPressed(new ClimbersUp(m_climber, Constants.climbToMid));
-    new JoystickButton(gunnerJS, 3).whenPressed(new ClimbersUp(m_climber, Constants.climbToRight));
     new JoystickButton(gunnerJS, 5).whenHeld(new TurretRotate(m_shooter, Constants.turretLeft));
     new JoystickButton(gunnerJS, 6).whenHeld(new TurretRotate(m_shooter, Constants.turretRight));
     new JoystickButton(gunnerJS, 7).whenHeld(new ShootandAimMid(m_shooter, m_conveyor));
